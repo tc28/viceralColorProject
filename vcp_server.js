a60d93cd9cf7c7386fcd8de51941d30e37afadb6
tc28 <tianc.19@gmail.com>
2014-04-11 20:40:03 -0400

    server almost done

--- vcp_server.js
+++ vcp_server.js
@@ -7,11 +7,24 @@ app.use(express.methodOverride());
 var anyDB = require('any-db');
 var conn = anyDB.createConnection('sqlite3://vcp.db');
 
+//rendering html 
+var engines = require('consolidate');
+app.engine('html', engines.hogan); //tell Express to run .html files through Hogan
+app.set('views', __dirname + '/prototype'); //tell Express where to find templates
+
+
 //I was wondering which methods that I should interact with the client
-app.get('/', function(request, response) {
-	//this is the function that is called
+app.get('/imageselection.html', function(request, response) {
+	//this is the function that is called when the user calls imageselections.html
+	response.render('imageselection.html', '');
+});
+
+app.get('/mobile.html', function(request, response) {
+	response.render('mobile.html', '');
 });
 
 
 
+
 app.listen(8000);
+console.log('server running on 8000');
