bb49c52db2b390866499173ec82678616d369e59
Avery Crits-Christoph <avery_crits-christoph@brown.edu>
2014-04-13 22:06:59 -0400

    Merge branch 'master' of https://github.com/jsl15/viceralColorProject
    Conflicts:
    	prototype/imageselection.html
    	prototype/mobile.html
    	prototype/stylesheet_prototype.css
    	prototype/website_page.html

--- vcp_server.js
+++ vcp_server.js
@@ -10,13 +10,18 @@ var conn = anyDB.createConnection('sqlite3://vcp.db');
 //rendering html 
 var engines = require('consolidate');
 app.engine('html', engines.hogan); //tell Express to run .html files through Hogan
-app.set('views', __dirname + '/prototype'); //tell Express where to find templates
+app.set('views', __dirname + '/templates'); //tell Express where to find templates
+app.use('/public', express.static('public'));
 
 app.get('/website_page.html', function(request, response) {
 	//this is the function that is called when the user calls website_page.html
 	response.render('website_page.html', '');
 });
 
+app.get('/', function(request, response) {
+	//this is the function that is called when the user calls imageselections.html
+	response.render('imageselection.html', '');
+});
 
 //I was wondering which methods that I should interact with the client
 app.get('/imageselection.html', function(request, response) {
